3f6df5a7cf7545ddaa30f9355fa921eea7153420
ismail <md.ismail.fakir@gmail.com>
2022-12-04 21:59:20 +0100

    autolink service discovery added

---
+++ src/test/java/net/cloudcentrik/autolink/eurekaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.cloudcentrik.autolink.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
